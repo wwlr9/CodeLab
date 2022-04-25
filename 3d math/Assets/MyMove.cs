3ec92acfa3221c64e1e8bb2aa8a8ffee27bd6500
wwlr9 <tiange.hou18@gmail.com>
2022-04-24 20:49:49 -0400

    3d math

---
+++ 3d math/Assets/MyMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMove : MonoBehaviour
+{
+    public float amplitute;
+    public float frequency;
+    public float timePoint;
+    public float timePoint2;
+    public float timePoint3;
+
+    public float click;//state of clicking
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amplitute = 7f;
+        frequency = 2f;
+        click = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePoint3 = Time.time;
+
+        float x = Mathf.Cos(timePoint * frequency) * amplitute;
+        float y = transform.position.y;
+        float z = transform.position.z;
+        
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            click = 1;
+            timePoint2 = Time.time; //constant: get the exact time when the button is released                      
+        }
+
+        if(click == 0)
+        {
+            timePoint = timePoint3; //variable: get a timer
+            
+        }
+        else
+        {
+            timePoint = timePoint2;
+            if (x <= 1.5f && x >= -1.5f)
+            {
+                Debug.Log("YOU GET IT");//Question: It seems like that the result is not accurate enough
+            }
+        }
+
+
+        transform.position = new Vector3(x, y, z);
+       
+
+    }
+}
